598100de9556905fc2e9bbfc883c48dcb96a5992
Jeff Camera <jeff@jeffcamera.com>
2016-12-28 17:42:03 -0500

    changed watch behavior

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,7 @@ function delayedWatch(globs, delegate) {
 	let timeout;
 	return gulp.watch(globs, event => {
 		util.log(`${util.colors[watchEventColors[event.type]](`[${event.type}]`)} ${util.colors.cyan(event.path)}`);
-		if (event.type !== 'added') {
+		if (event.type === 'changed' || event.type === 'renamed') {
 			clearTimeout(timeout);
 			timeout = setTimeout(delegate, 500);
 		}
