83b8975ed1fb054984453f85ea7896ba236b967f
Jeff Camera <jeff@jeffcamera.com>
2017-01-30 18:06:59 -0500

    cleaned up unused BrowserUser code

--- src/browser/BrowserUser.ts
+++ src/browser/BrowserUser.ts
@@ -1,28 +1,3 @@
 import User from '../common/User';
-import UserAccount from '../common/api/models/UserAccount';
 
-export default class BrowserUser extends User {
-	//sprivate _extensionId = 'ibdjhkiiiiifdgmdalkofacfnihpomkn';
-	constructor(userAccount: UserAccount) {
-		super(userAccount);
-	}
-	// private getSessionKey() {
-	// 	return document.cookie.match(/(?:(?:^|.*;\s*)sessionKey\s*\=\s*([^;]*).*$)|^.*$/)[1];
-	// }
-	public signIn(userAccount: UserAccount) {
-		const result = super.signIn(userAccount);
-		// chrome.runtime.sendMessage(this._extensionId, {
-		// 	type: 'signIn',
-		// 	data: {
-		// 		userAccount: this.getUserAccount(),
-		// 		sessionKey: this.getSessionKey()
-		// 	}
-		// });
-		return result;
-	}
-	public signOut() {
-		const result = super.signOut();
-		//chrome.runtime.sendMessage(this._extensionId, { type: 'signOut' });
-		return result;
-	}
-}
+export default class BrowserUser extends User {}
