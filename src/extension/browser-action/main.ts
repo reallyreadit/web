411db220e52cded300a739f8546d62031f093486
Jeff Camera <Jeff Camera>
2017-02-13 19:30:45 -0500

    added % complete

--- src/extension/browser-action/main.ts
+++ src/extension/browser-action/main.ts
@@ -4,6 +4,7 @@ console.log('loading main.ts...');
 
 const authCheckbox = document.getElementsByName('authenticated')[0] as HTMLInputElement,
 	  articleTitleCell = document.querySelector('td[data-name="articleTitle"]') as HTMLTableCellElement,
+	  percentCompleteCell = document.querySelector('td[data-name="percentComplete"]') as HTMLTableCellElement,
 	  overlayCheckbox = document.getElementsByName('showOverlay')[0] as HTMLInputElement;
 
 const eventPageApi = new EventPageApi();
@@ -14,6 +15,8 @@ eventPageApi
 		authCheckbox.checked = state.isAuthenticated;
 		articleTitleCell.innerText = state.userArticle ? state.userArticle.title : 'N/A',
 		articleTitleCell.classList.toggle('null', !state.userArticle);
+		percentCompleteCell.innerText = state.userArticle ? state.userArticle.percentComplete + '%' : 'N/A';
+		percentCompleteCell.classList.toggle('null', !state.userArticle);
 		overlayCheckbox.checked = state.showOverlay;
 	});
 
