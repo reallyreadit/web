6d13e8e52c24cbc26333bef42fed944ec03167fe
Jeff Camera <Jeff Camera>
2017-04-15 18:58:43 -0400

    updated browser action layout!

--- src/extension/browser-action/main.ts
+++ src/extension/browser-action/main.ts
@@ -1,23 +1,30 @@
 import EventPageApi from './EventPageApi';
+import readingParameters from '../../common/readingParameters';
 
 console.log('loading main.ts...');
 
-const authCheckbox = document.getElementsByName('authenticated')[0] as HTMLInputElement,
-	  articleTitleCell = document.querySelector('td[data-name="articleTitle"]') as HTMLTableCellElement,
-	  percentCompleteCell = document.querySelector('td[data-name="percentComplete"]') as HTMLTableCellElement,
-	  overlayCheckbox = document.getElementsByName('showOverlay')[0] as HTMLInputElement;
-
-const eventPageApi = new EventPageApi();
+const eventPageApi = new EventPageApi(),
+	overlayCheckbox = document.getElementById('show-overlay') as HTMLInputElement;
 
 eventPageApi
 	.getState()
 	.then(state => {
-		authCheckbox.checked = state.isAuthenticated;
-		articleTitleCell.innerText = state.userArticle ? state.userArticle.title : 'N/A',
-		articleTitleCell.classList.toggle('null', !state.userArticle);
-		percentCompleteCell.innerText = state.userArticle ? state.userArticle.percentComplete + '%' : 'N/A';
-		percentCompleteCell.classList.toggle('null', !state.userArticle);
+		if (state.userArticle) {
+			const slugParts = state.userArticle.slug.split('_'),
+				percentCompleteLabel = document.getElementById('percent-complete'),
+				commentsAnchor = document.getElementById('comments') as HTMLAnchorElement;
+			document.getElementById('article-title').textContent = state.userArticle.title.length > 48 ?
+				state.userArticle.title.substr(0, 48).trim() + '...' :
+				state.userArticle.title;
+			percentCompleteLabel.classList.toggle('unlocked', state.userArticle.percentComplete >= readingParameters.articleUnlockThreshold);
+			percentCompleteLabel.firstElementChild.textContent = state.userArticle.percentComplete.toFixed() + '%';
+			commentsAnchor.href = `${config.web.protocol}://${config.web.host}/articles/${slugParts[0]}/${slugParts[1]}`;
+			commentsAnchor.innerText = state.userArticle.commentCount + ' ' + (state.userArticle.commentCount === 1 ? 'comment' : 'comments');
+			document.getElementById('article-placeholder').style.display = 'none';
+		} else {
+			document.getElementById('article-info').style.display = 'none';
+		}
 		overlayCheckbox.checked = state.showOverlay;
 	});
 
-overlayCheckbox.addEventListener('change', e => eventPageApi.updateShowOverlay((e.target as HTMLInputElement).checked));
+overlayCheckbox.addEventListener('change', e => eventPageApi.updateShowOverlay((e.currentTarget as HTMLInputElement).checked));
--- src/extension/common/UserArticle.ts
+++ src/extension/common/UserArticle.ts
@@ -1,6 +1,18 @@
 interface UserArticle {
 	id: string,
 	title: string,
+	slug: string,
+	sourceId: string,
+	datePublished: string,
+	dateModified: string,
+	section: string,
+	description: string,
+	source: string,
+	url: string,
+	authors: string[],
+	tags: string[],
+	wordCount: number,
+	pageCount: number,
 	percentComplete: number,
 	commentCount: number
 }
