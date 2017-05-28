2f06318a271a356d9817e4506085c5e5b7c5e2ac
Jeff Camera <jeff@jeffcamera.com>
2017-05-28 12:48:11 -0400

    added about page content

--- src/app/common/components/AboutPage.tsx
+++ src/app/common/components/AboutPage.tsx
@@ -11,7 +11,22 @@ export default class AboutPage extends PureContextComponent<{}, {}> {
 	public render() {
 		return (
 			<div className="about-page">
-				<span>About us!</span>
+				<p>
+					<strong>The internet kind of sucks.</strong> Echo chambers. Fake news. Trolls. The nagging suspicion that we’re screaming into a void.
+				</p>
+				<p>
+					Reallyread.it allows you to talk to other people who have really read the same articles that you’ve really read. And, more importantly, we block everyone else.
+				</p>
+				<p>
+					<strong>All voices are not created equal.<br />
+					Commenting is a privilege, not a right.</strong>
+				</p>
+				To get started:
+				<ul>
+					<li>Create an account</li>
+					<li>Download the extension</li>
+					<li>Your reading history is saved (privately!) on reallyread.it and you can join in on conversations about the articles and stories you’ve really read.</li>
+				</ul>
 			</div>
 		);
 	}
