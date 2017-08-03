3503b20267dc4ca2311435984bbf088209d336c1
Jeff Camera <jeff@jeffcamera.com>
2017-08-02 23:06:44 -0400

    added warning triangle svg

---
+++ src/common/svg/warningTriangle.ts
@@ -0,0 +1,14 @@
+export default
+`<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 58">
+  <g
+     id="layer1">
+    <path
+       id="path4493"
+       d="M 31.982422 2.03125 L 16.986328 28.982422 L 1.9902344 55.933594 L 31.982422 55.933594 L 61.976562 55.933594 L 46.980469 28.982422 L 31.982422 2.03125 z "
+       style="opacity:1;fill:#ffff00;fill-opacity:1;stroke:#000000;stroke-width:4.05618858;stroke-linejoin:round;stroke-miterlimit:4;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1" />
+    <path
+       id="path4499"
+       d="M 31.982422 21.8125 C 29.292179 21.8125 27.947266 22.843117 27.947266 24.904297 C 27.947266 25.14521 27.967659 25.386033 28.007812 25.626953 L 30.478516 39.982422 L 33.488281 39.982422 L 35.958984 25.626953 C 35.999138 25.412806 36.019531 25.165839 36.019531 24.884766 C 36.019531 22.836972 34.672665 21.8125 31.982422 21.8125 z M 31.982422 42.933594 C 29.626786 42.933594 28.449219 44.111157 28.449219 46.466797 C 28.449219 48.82243 29.626786 50 31.982422 50 C 34.338057 50 35.517578 48.82243 35.517578 46.466797 C 35.517578 44.111157 34.338057 42.933594 31.982422 42.933594 z "
+       style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:32px;line-height:1.25;font-family:'Britannic Bold';-inkscape-font-specification:'Britannic Bold, ';letter-spacing:0px;word-spacing:0px;fill:#000000;fill-opacity:1;stroke:none;stroke-width:1.2848922" />
+  </g>
+</svg>`;
