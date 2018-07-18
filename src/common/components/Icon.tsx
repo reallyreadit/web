9f354ac10a488b7cf979620b7ecfe864c780e1fa
Jeff <jeff@jeffcamera.com>
2018-07-18 12:56:15 -0400

    added new icons

--- src/common/components/Icon.tsx
+++ src/common/components/Icon.tsx
@@ -5,7 +5,8 @@ export type IconName = 'locked' | 'switch' | 'plus' | 'refresh2' | 'checkmark' |
 	'cancel' | 'backward' | 'exclamation' | 'forbid' | 'spinner' | 'comments' |
 	'write' | 'envelope' | 'user' | 'cog' | 'book' | 'refresh' | 'lightbulb' |
 	'question' | 'hyperlink' | 'gallery' | 'star' | 'clock' | 'key' | 'trophy' |
-	'home' | 'box' | 'star-empty' | 'email' | 'share';
+	'home' | 'box' | 'star-empty' | 'email' | 'share' | 'in-alt' | 'out-alt' |
+	'chevron-down' | 'chevron-up';
 export default (props: {
 	name: IconName,
 	title?: string,
