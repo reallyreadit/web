6ffeedf3cbc9b22355131b68c20eef4b1b4357d5
Jeff <jeff@jeffcamera.com>
2018-07-18 12:54:30 -0400

    added subtitle to Page

--- src/app/common/components/Page.tsx
+++ src/app/common/components/Page.tsx
@@ -3,12 +3,18 @@ import * as className from 'classnames';
 
 export default (props: {
 	title?: string,
+	subTitle?: string,
 	children: React.ReactNode,
 	className?: ClassValue
 }) => (
 	<div className={className('page', props.className)}>
 		{props.title ?
-			<div className="title">{props.title}</div> :
+			<div className="title">
+				<span>{props.title}</span>
+			</div> :
+			null}
+		{props.subTitle ?
+			<div className="sub-title">{props.subTitle}</div> :
 			null}
 		{props.children}
 	</div>
