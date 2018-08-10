16b8a595347853e2dc1eab6bbbabfb0c3305780c
Jeff <jeff@jeffcamera.com>
2018-08-10 13:11:17 -0400

    messaging cleanup

--- src/app/browser/WebViewMessagingContext.ts
+++ src/app/browser/WebViewMessagingContext.ts
@@ -1,16 +1,15 @@
-type OnMessageListener = (data: any, messageSender: null, sendResponse: (data: any) => void) => void;
 export interface AppApi {
 	sendResponse: (jsonCallbackResponse: string) => void,
 	postMessage: (jsonMessage: string) => void
 }
+type OnMessageListener = (data: any, messageSender: null, sendResponse: (data: any) => void) => void;
 interface ResponseCallback {
 	id: number,
 	function: (data: any) => void
 }
 export default class {
-	private _responseCallbackId = 0;
-	private readonly _responseCallbacks: ResponseCallback[] = [];
 	private readonly _onMessageListeners: OnMessageListener[] = [];
+	private readonly _responseCallbacks: ResponseCallback[] = [];
 	constructor() {
 		window.reallyreadit = {
 			sendResponse: (jsonCallbackResponse: string) => {
@@ -26,7 +25,12 @@ export default class {
 				const message = JSON.parse(jsonMessage);
 				let sendResponse: (data: any) => void;
 				if (message.callbackId != null) {
-					sendResponse = data => window.postMessage(JSON.stringify({ id: message.callbackId, data }), '*');
+					sendResponse = response => {
+						this.postMessage({
+							id: message.callbackId,
+							data: response
+						});
+					}
 				} else {
 					sendResponse = () => {};
 				}
@@ -36,18 +40,22 @@ export default class {
 			}
 		};
 	}
+	private postMessage(message: { data: any, callbackId: number | null } | { data: any, id: number }) {
+		window.postMessage('reallyreadit:' + JSON.stringify(message), '*');
+	}
 	public addListener(listener: OnMessageListener) {
 		this._onMessageListeners.push(listener);
 	}
-	public sendMessage(message: {}): void;
-	public sendMessage(message: {}, responseCallback: (data: any) => void | null = null) {
+	public sendMessage(message: any, responseCallback?: (message: any) => void) {
 		let callbackId: number | null = null;
 		if (responseCallback) {
 			this._responseCallbacks.push({
-				id: (callbackId = this._responseCallbackId++),
+				id: callbackId = this._responseCallbacks.length ?
+					Math.max(...this._responseCallbacks.map(callback => callback.id)) + 1 :
+					0,
 				function: responseCallback
 			});
 		}
-		window.postMessage(JSON.stringify({ data: message, callbackId }), '*');
+		this.postMessage({ data: message, callbackId });
 	}
 }
