293156be5e0b88f8e7a76aee7bc7b0e451e3e04e
jeff <jeff@jeffcamera.com>
2018-08-13 18:40:09 -0400

    added MobileButton

---
+++ src/app/common/components/MainView/AppAuthScreen/MobileButton.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import * as className from 'classnames';
+import Spinner from '../../../../../common/components/Spinner';
+
+export default (props: {
+	busy?: boolean,
+	onClick: () => void,
+	style?: 'loud',
+	text: string
+}) => (
+	<button
+		className={className('mobile-button', {
+			'busy': props.busy,
+			'loud': props.style === 'loud'
+		})}
+		disabled={props.busy}
+		onClick={props.onClick}
+	>
+		{props.busy ?
+			<Spinner /> :
+			null}
+		{props.text}
+		{props.busy ?
+			<Spinner /> :
+			null}
+	</button>
+);
