025737040633577e0d6005732d6795cf62cbe870
Jeff <jeff@jeffcamera.com>
2019-02-08 14:23:22 -0500

    removed parseMetadata from ContentScriptInitData

--- src/extension/common/ContentScriptInitData.ts
+++ src/extension/common/ContentScriptInitData.ts
@@ -4,7 +4,6 @@ import SourceRule from '../../common/models/SourceRule';
 export default interface ContentScriptInitData {
 	config: ContentScriptConfig,
 	loadPage: boolean,
-	parseMetadata: boolean,
 	parseMode: 'analyze' | 'mutate',
 	showOverlay: boolean,
 	sourceRules: SourceRule[]
