89e1bcf5a289e2e102b1c5f55dc86e363c7b1c20
Jeff <jeff@jeffcamera.com>
2019-03-08 10:56:49 -0500

    added SemanticVersion

---
+++ src/common/SemanticVersion.ts
@@ -0,0 +1,38 @@
+export default class SemanticVersion {
+	private readonly _major: number;
+	private readonly _minor: number;
+	private readonly _patch: number;
+	constructor(version: string) {
+		const match = version.match(/(\d+)\.(\d+)\.(\d+)/);
+		if (!match) {
+			throw new Error('Invalid version format');
+		}
+		this._major = parseInt(match[1]);
+		this._minor = parseInt(match[2]);
+		this._patch = parseInt(match[3]);
+	}
+	public compareTo(version: SemanticVersion) {
+		if (this._major !== version._major) {
+			return this._major - version._major;
+		}
+		if (this._minor !== version._minor) {
+			return this._minor - version._minor;
+		}
+		if (this._patch !== version._patch) {
+			return this._patch - version._patch;
+		}
+		return 0;
+	}
+	public toString() {
+		return `${this._major}.${this._minor}.${this._patch}`;
+	}
+	public get major() {
+		return this._major;
+	}
+	public get minor() {
+		return this._minor;
+	}
+	public get patch() {
+		return this._patch;
+	}
+}
