81b5934bc39e04686182fe6a3498315353ef3272
Jeff <jeff@jeffcamera.com>
2019-03-24 16:56:36 -0400

    fixed extension manifest version formatting

--- build/targets/extension.js
+++ build/targets/extension.js
@@ -26,7 +26,7 @@ const
 						.readFileSync('./package.json')
 						.toString()
 				);
-			manifest.version = package['it.reallyread'].version.extension.toString();
+			manifest.version = package['it.reallyread'].version.extension.toString().padEnd(4, '0');
 			fs.writeFileSync(
 				fileName,
 				JSON.stringify(manifest, null, 3)
