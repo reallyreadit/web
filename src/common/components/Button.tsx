562ae4f64b005850bd37c48844b5ee16af48f537
Jeff <jeff@jeffcamera.com>
2019-04-08 21:55:36 -0400

    updated common button

--- src/common/components/Button.tsx
+++ src/common/components/Button.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react';
 import classNames from 'classnames';
 import Icon, { IconName } from './Icon';
+import Spinner from './Spinner';
 
 export default class Button extends React.PureComponent<{
 	text: string,
 	iconLeft?: IconName,
-	contentLeft?: React.ReactNode,
 	style?: 'normal' | 'preferred',
-	state?: 'normal' | 'active' | 'disabled' | 'busy',
+	state?: 'normal' | 'disabled' | 'busy',
 	showIndicator?: boolean,
-	textStyle?: 'normal' | 'error'
 	onClick?: () => void
 }, {}> {
 	private handleClick = () => {
@@ -24,25 +23,19 @@ export default class Button extends React.PureComponent<{
 		const classList = {
 			preferred: this.props.style === 'preferred',
 			disabled: this.props.state === 'disabled' || this.props.state === 'busy',
-			busy: this.props.state === 'busy',
-			active: this.props.state === 'active'
+			indicator: this.props.showIndicator
 		};
 		return (
 			<button className={classNames('button_ovrlmi', classList)} onClick={this.handleClick}>
-				<div className="outer-wrap">
-					<div className="inner-wrap">
-						<div className="content">
-							{this.props.iconLeft ?
-								<span className={classNames('icon-wrapper', { 'indicator': this.props.showIndicator })}><Icon name={this.props.iconLeft} /></span> :
-								null}
-							{this.props.contentLeft ?
-								<div className="content-left">{this.props.contentLeft}</div> :
-								null}
-							<span className={classNames('text', { 'error': this.props.textStyle === 'error' })}>{this.props.text}</span>
-							<div className="loading"></div>
-						</div>
-					</div>
-				</div>
+				{this.props.iconLeft ?
+					<Icon name={this.props.iconLeft} /> :
+					null}
+				<span className="text">{this.props.text}</span>
+				{this.props.state === 'busy' ?
+					<span className="loading">
+						<Spinner />
+					</span> :
+					null}
 			</button>
 		);
 	}
