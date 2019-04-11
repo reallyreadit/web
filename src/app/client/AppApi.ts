44c5440f180930e6b37c37f8cccd9b6852063cc6
Jeff <jeff@jeffcamera.com>
2019-04-11 16:32:36 -0400

    removed unused 'fetch' message handler

--- src/app/client/AppApi.ts
+++ src/app/client/AppApi.ts
@@ -9,27 +9,12 @@ export default class extends AppApi {
 	constructor(messagingContext: WebViewMessagingContext) {
 		super();
 		this._messagingContext = messagingContext;
-		messagingContext.addListener((message: { type: string, data: any }, sender, sendResponse) => {
+		messagingContext.addListener(message => {
 			switch (message.type) {
 				case 'articleUpdated':
 					this.emitEvent('articleUpdated', message.data);
-					return false;
-				case 'fetch':
-					const init: RequestInit = {
-						method: message.data.method,
-						credentials: 'include'
-					};
-					if (message.data.data) {
-						init.headers = { 'Content-Type': 'application/json' };
-						init.body = JSON.stringify(message.data.data);
-					}
-					fetch(message.data.uri, init)
-						.then(res => res.json())
-						.then(data => sendResponse({ data }))
-						.catch(error => sendResponse({ error }));
-					return true;
+					break;
 			}
-			return false;
 		});
 		messagingContext.sendMessage(
 			{
