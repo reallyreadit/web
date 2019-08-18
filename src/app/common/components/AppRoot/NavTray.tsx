f582f8633e80fb6db3a5329448be9ab56abb6bf8
Jeff <jeff@jeffcamera.com>
2019-08-18 14:08:48 -0400

    fixed viewProfile handler

--- src/app/common/components/AppRoot/NavTray.tsx
+++ src/app/common/components/AppRoot/NavTray.tsx
@@ -7,8 +7,7 @@ import { findRouteByKey } from '../../../../common/routing/Route';
 import { Screen } from '../Root';
 
 const profileRoute = findRouteByKey(routes, ScreenKey.Profile);
-
-export default (props: {
+interface Props {
 	onViewHome: () => void,
 	onViewLeaderboards: () => void,
 	onViewMyReads: () => void,
@@ -16,59 +15,68 @@ export default (props: {
 	onViewStats: () => void,
 	selectedScreen: Screen,
 	user: UserAccount
-}) => (
-	<ol className="nav-tray_2tc8">
-		<li>
-			<button
-				className={props.selectedScreen.key === ScreenKey.Home ? 'selected' : null}
-				onClick={props.onViewHome}
-			>
-				<Icon name="earth" />
-				<label>Discover</label>
-			</button>
-		</li>
-		<li>
-			<button
-				className={props.selectedScreen.key === ScreenKey.MyReads ? 'selected' : null}
-				onClick={props.onViewMyReads}
-			>
-				<Icon name="star" />
-				<label>My Reads</label>
-			</button>
-		</li>
-		<li>
-			<button
-				className={
-					(
-						props.selectedScreen.key === ScreenKey.Profile &&
-						profileRoute.getPathParams(props.selectedScreen.location.path)['userName'].toLowerCase() === props.user.name.toLowerCase()
-					) ?
-						'selected' :
-						null
-				}
-				onClick={props.onViewProfile}
-			>
-				<Icon name="user" />
-				<label>Profile</label>
-			</button>
-		</li>
-		<li>
-			<button
-				className={props.selectedScreen.key === ScreenKey.Stats ? 'selected' : null}
-				onClick={props.onViewStats}
-			>
-				<Icon name="line-chart" />
-				<label>Stats</label>
-			</button>
-		</li>
-		<li>
-			<button
-				className={props.selectedScreen.key === ScreenKey.Leaderboards ? 'selected' : null}
-				onClick={props.onViewLeaderboards}
-			>
-				<Icon name="podium" />
-				<label>Leaderboards</label>
-			</button>
-		</li>
-	</ol>
-);
+}
+export default class NavTray extends React.PureComponent<Props>{
+	// needed so that the <button> this ref isn't passed as a parameter
+	private readonly _viewProfile = () => {
+		this.props.onViewProfile();
+	};
+	public render() {
+		return (
+			<ol className="nav-tray_2tc8">
+				<li>
+					<button
+						className={this.props.selectedScreen.key === ScreenKey.Home ? 'selected' : null}
+						onClick={this.props.onViewHome}
+					>
+						<Icon name="earth" />
+						<label>Discover</label>
+					</button>
+				</li>
+				<li>
+					<button
+						className={this.props.selectedScreen.key === ScreenKey.MyReads ? 'selected' : null}
+						onClick={this.props.onViewMyReads}
+					>
+						<Icon name="star" />
+						<label>My Reads</label>
+					</button>
+				</li>
+				<li>
+					<button
+						className={
+							(
+								this.props.selectedScreen.key === ScreenKey.Profile &&
+								profileRoute.getPathParams(this.props.selectedScreen.location.path)['userName'].toLowerCase() === this.props.user.name.toLowerCase()
+							) ?
+								'selected' :
+								null
+						}
+						onClick={this._viewProfile}
+					>
+						<Icon name="user" />
+						<label>Profile</label>
+					</button>
+				</li>
+				<li>
+					<button
+						className={this.props.selectedScreen.key === ScreenKey.Stats ? 'selected' : null}
+						onClick={this.props.onViewStats}
+					>
+						<Icon name="line-chart" />
+						<label>Stats</label>
+					</button>
+				</li>
+				<li>
+					<button
+						className={this.props.selectedScreen.key === ScreenKey.Leaderboards ? 'selected' : null}
+						onClick={this.props.onViewLeaderboards}
+					>
+						<Icon name="podium" />
+						<label>Leaderboards</label>
+					</button>
+				</li>
+			</ol>
+		);
+	}
+}
