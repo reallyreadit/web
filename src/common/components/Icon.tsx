53108c7c1462579d8b7a1a3b791bfe60652814c9
Jeff <jeff@jeffcamera.com>
2019-09-20 12:52:59 -0400

    added badge to icon

--- src/common/components/Icon.tsx
+++ src/common/components/Icon.tsx
@@ -1,34 +1,31 @@
 import * as React from 'react';
 import classNames, { ClassValue } from 'classnames';
 
-export type IconName = 'article-details-star' | 'backward' | 'binoculars' | 'bookmark' | 'box' | 'cancel' | 'checkmark' | 'chevron-down' | 'chevron-left' | 'clock' | 'comments' | 'earth' | 'email' | 'exclamation' | 'fire' | 'forbid' | 'graduation' | 'line-chart' | 'link' | 'locked' | 'medal' | 'menu2' | 'paper-plane' | 'plus' | 'podium' | 'question-circle' | 'quill' | 'refresh' | 'refresh2' | 'share' | 'spinner' | 'star' | 'trophy' | 'twitter' | 'user' | 'warning' | 'write';
+export type IconName = 'article-details-star' | 'backward' | 'bell' | 'binoculars' | 'bookmark' | 'box' | 'cancel' | 'checkmark' | 'chevron-down' | 'chevron-left' | 'clock' | 'comments' | 'earth' | 'email' | 'exclamation' | 'fire' | 'forbid' | 'graduation' | 'line-chart' | 'link' | 'locked' | 'medal' | 'menu2' | 'paper-plane' | 'plus' | 'podium' | 'question-circle' | 'quill' | 'refresh' | 'refresh2' | 'share' | 'spinner' | 'star' | 'trophy' | 'twitter' | 'user' | 'warning' | 'write';
 export default (props: {
+	badge?: number,
 	display?: 'inline' | 'block',
 	name: IconName,
 	title?: string,
 	className?: ClassValue,
-	onClick?: () => void,
-	defs?: React.ReactNode,
-	fill?: string
+	onClick?: () => void
 }) => (
-	<svg
+	<div
 		className={
 			classNames(
 				'icon_rour3d',
 				props.className,
-				props.display
+				props.display || 'inline'
 			)
 		}
 		onClick={props.onClick}
 	>
-		{props.defs ?
-			<defs>
-				{props.defs}
-			</defs> :
-			null}
-		{props.title ?
-			<title>{props.title}</title> :
-			null}
-		<use xlinkHref={'#icon-' + props.name} fill={props.fill}></use>
-	</svg>
+		<span className={classNames('badge', { 'hidden': !props.badge })}>{props.badge}</span>
+		<svg className="icon">
+			{props.title ?
+				<title>{props.title}</title> :
+				null}
+			<use xlinkHref={'#icon-' + props.name}></use>
+		</svg>
+	</div>
 );
