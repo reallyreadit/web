1f01f1fdfacc53c4c9d253d5171d9dfb711a3389
Jeff <jeff@jeffcamera.com>
2020-02-13 16:53:33 -0500

    updated marketing copy

--- src/app/common/marketingTesting.ts
+++ src/app/common/marketingTesting.ts
@@ -4,16 +4,8 @@ export const variants: {
 		subtext: string
 	}
 } = {
-	4: {
-		headline: 'Join our community of readers.',
-		subtext: 'Find and share the best articles on the web.'
-	},
-	5: {
-		headline: 'Vote with your attention.',
-		subtext: 'Social media for readers, writers, and free-thinkers.'
-	},
-	6: {
-		headline: 'Read past the headlines.',
-		subtext: 'Your favorite articles and stories, 100% distraction-free.'
+	7: {
+		headline: 'Social media powered by reading.',
+		subtext: 'The best way to find, read and share articles online.'
 	}
 };
