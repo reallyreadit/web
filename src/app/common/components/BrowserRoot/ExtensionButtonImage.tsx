80d77b13b90c227fb1c25ddca403f4c243a22ce4
Jeff <jeff@jeffcamera.com>
2020-04-05 17:01:13 -0400

    factored out ExtensionButtonImage

---
+++ src/app/common/components/BrowserRoot/ExtensionButtonImage.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { DeviceType } from '../../../../common/DeviceType';
+
+export default (
+	props: {
+		deviceType: DeviceType
+	}
+) => {
+	let imgUrl = '/images/';
+	if (props.deviceType === DeviceType.DesktopFirefox) {
+		imgUrl += 'bai-screenshot-firefox.png';
+	} else {
+		imgUrl += 'bai-screenshot-chrome.png';
+	}
+	return (
+		<img
+			alt="Readup Extension Button"
+			className="extension-button-image_dr3we4"
+			src={imgUrl}
+		/>
+	);
+};
--- src/app/common/components/BrowserRoot/ExtensionReminderDialog.tsx
+++ src/app/common/components/BrowserRoot/ExtensionReminderDialog.tsx
@@ -1,30 +1,23 @@
 import * as React from 'react';
 import Dialog from '../../../../common/components/Dialog';
 import { DeviceType } from '../../../../common/DeviceType';
+import ExtensionButtonImage from './ExtensionButtonImage';
 
 export default (
 	props: {
 		deviceType: DeviceType,
 		onSubmit: () => Promise<void>
 	}
-) => {
-	let imgUrl = '/images/';
-	if (props.deviceType === DeviceType.DesktopFirefox) {
-		imgUrl += 'bai-screenshot-firefox.png';
-	} else {
-		imgUrl += 'bai-screenshot-chrome.png';
-	}
-	return (
-		<Dialog
-			className="extension-reminder-dialog_i546dx"
-			onSubmit={props.onSubmit}
-			size="small"
-			submitButtonText="Got it"
-			textAlign="center"
-			title="Don't Forget"
-		>
-			<p>Click the Readup button to turn on Reader-mode. Otherwise you won't get credit.</p>
-			<img src={imgUrl} alt="Readup Extension Button" />
-		</Dialog>
-	);
-}
+) => (
+	<Dialog
+		className="extension-reminder-dialog_i546dx"
+		onSubmit={props.onSubmit}
+		size="small"
+		submitButtonText="Got it"
+		textAlign="center"
+		title="Don't Forget"
+	>
+		<p>Click the Readup button to turn on Reader-mode. Otherwise you won't get credit.</p>
+		<ExtensionButtonImage deviceType={props.deviceType} />
+	</Dialog>
+);
