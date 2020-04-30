818bb9c660d39e66f01079a215d115bb707dbbee
Jeff <jeff@jeffcamera.com>
2020-04-30 13:11:02 -0400

    added extension source publisher script

---
+++ publish-extension-source.ps1
@@ -0,0 +1,28 @@
+$targetDir = $args[0]
+
+if (-not (Test-Path $targetDir)) {
+	Write-Host 'Target directory not found'
+	Exit
+}
+
+Write-Host 'Copying build scripts...'
+Robocopy.exe build $(Join-Path $targetDir 'build') /MIR /XF client.js server.js /XD client nativeClient
+
+Write-Host 'Copying source files...'
+Robocopy.exe src/common $(Join-Path $targetDir 'src/common') /MIR
+Robocopy.exe src/extension $(Join-Path $targetDir 'src/extension') /MIR
+
+Write-Host 'Copying root files...'
+Copy-Item npm-shrinkwrap.json $targetDir
+Copy-Item package.json $targetDir
+Copy-Item tsconfig.json $targetDir
+
+Write-Host 'Running build...'
+Push-Location $targetDir
+npm install
+node node_modules/gulp/bin/gulp.js build:prod:extension
+
+Write-Host 'Creating zip file...'
+Compress-Archive -Path build,src,gulpfile.js,npm-shrinkwrap.json,package.json,readme.md,tsconfig.json -DestinationPath archive.zip
+
+Pop-Location
