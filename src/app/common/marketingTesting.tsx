bb8e7810e9824097349eeda27a7798d9dad19cc2
Jeff <jeff@jeffcamera.com>
2020-10-14 13:45:37 -0400

    updated marketing copy

--- src/app/common/marketingTesting.tsx
+++ src/app/common/marketingTesting.tsx
@@ -7,18 +7,6 @@ export const variants: {
 	}
 } = {
 	0: {
-		headline: 'Deep reading is screen time well spent.',
-		subtext: (
-			<>
-				Readup offers distraction-free reading and noise-cancelled comments, built <em>by</em> and <em>for</em> readers. The future is focus. The future is slow. Join us.
-			</>
-		)
-	},
-	1: {
-		headline: 'Stop feeding. Start reading.',
-		subtext: 'Readup helps you spend more of your time and attention on deep, focused reading. We are a community of readers, writers and free-thinkers. Join us today!'
-	},
-	2: {
 		headline: 'We are a community of readers. Join us!',
 		subtext: 'Readup is a social reading platform. No ads. No distractions. No liking or upvotes. We help you pay attention to what matters: reading.'
 	}
