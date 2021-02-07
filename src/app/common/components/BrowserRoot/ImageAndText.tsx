ed26c6853e8bb36ed25d0d9f0c1fa399c39a4050
Thor Galle <thorgalle@gmail.com>
2021-02-07 12:22:38 +0100

    started extracting reusable font styles

--- src/app/common/components/BrowserRoot/ImageAndText.tsx
+++ src/app/common/components/BrowserRoot/ImageAndText.tsx
@@ -20,7 +20,7 @@ export default (
 		<div className={classNames("image-text__wrapper", props.imageRight ? "image-text__wrapper--image-right" : "", "content", )}>
 			<img className="image-text__image" src="/images/read-anything.png" alt=""/>
 			<div className="image-text__text">
-				<h2 className="image-text__heading">Value point header</h2>
+				<h2 className={classNames("image-text__heading", "heading-small")}>Value point header</h2>
 				<p>Ad labore esse cillum nulla voluptate nulla et quis ut reprehenderit esse nisi. Ut aliquip consectetur dolor commodo ad nisi eiusmod. Non sunt anim eu sint tempor ad nisi duis cillum sunt ad. Labore aute ea id nulla aute dolore. Enim in excepteur deserunt ad.</p>
 			</div>
 		</div>
