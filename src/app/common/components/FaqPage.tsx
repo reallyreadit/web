04ff740b14d882b1275ce11b4d6b57e01ca37d03
Thor Galle <thorgalle@gmail.com>
2021-03-21 13:08:37 +0100

    misc changes, first content fill in FaqPage

--- src/app/common/components/FaqPage.tsx
+++ src/app/common/components/FaqPage.tsx
@@ -17,26 +17,27 @@ interface FaqCategory {
 
 const faqs: FaqCategory[] = [
 	{
-		title: "Installing Readup",
+		title: "Getting Started",
 		questions: [
 			{
-				question: "Is Readup coming to Android?",
-				answer: <p>Maybe. Do you want a cookie?</p>
+				question: "How do I get started on my iPhone?",
+				answer: <p>Download <a href="https://apps.apple.com/us/app/readup-social-reading/id1441825432">the app</a>&nbsp;
+						from the app store.</p>
 			},
 			{
-				question: "Can we fly to mars yet?",
-				answer: <p>Maybe. Do you want a cookie?</p>
+				question: "How do I get started on my laptop or computer?",
+				answer: <p>First, add the Readup browser extension to your favorite web browser —&nbsp;
+					<a href="https://chrome.google.com/webstore/detail/readup/mkeiglkfdfamdjehidenkklibndmljfi?hl=en-US">Chrome</a>,&nbsp;
+					<a href="https://addons.mozilla.org/en-US/firefox/addon/readup/">Firefox</a>,&nbsp;
+					<a href="https://apps.apple.com/us/app/readup-social-reading/id1441825432">Safari</a> or&nbsp;
+					<a href="https://microsoftedge.microsoft.com/addons/detail/readup/nnnlnihiejbbkikldbfeeefljhpplhcm">Edge</a>.&nbsp;
+					 Next, look for the Readup button in your browser window (near the URL bar) which enables you to activate Reader Mode&nbsp;
+					 on any article you wish to read with just one click.
+					 <br/>Finally, remember to visit Readup.com to explore articles, read comments, and manage your account.</p>
 			},
 			{
-				question: "How far is asgaard?",
-				answer: <>
-					<p>Lorem id in sunt eiusmod. Incididunt eu officia anim duis minim pariatur quis minim. Lorem aliqua esse Lorem eiusmod. Do adipisicing nulla minim qui commodo qui velit eu nostrud ipsum magna labore. Labore esse Lorem do amet occaecat. Voluptate consequat laborum esse amet in sit ea do enim. Sint nisi magna est ex esse.</p>
-					<p>Adipisicing ut duis et ea nisi do eu labore velit proident ex. Ullamco proident do consequat dolore duis duis. Dolore excepteur labore reprehenderit exercitation ex. Velit cillum laboris enim nulla cillum consequat voluptate do.</p>
-				</>
-			},
-			{
-				question: "Do I have to pay?",
-				answer: <p>Maybe. Do you want a cookie?</p>
+				question: "Do I need to create an account?",
+				answer: <p>Yes. In addition to downloading the app and/or extensions, you will need to create an account on Readup.</p>
 			},
 		]
 	},
@@ -44,21 +45,100 @@ const faqs: FaqCategory[] = [
 		title: "Importing articles",
 		questions: [
 			{
-				question: "Is Readup coming to Android?",
-				answer: <p>Maybe. Do you want a cookie?</p>
+				question: "Do I need to import articles?",
+				answer: <p>Nope. You can always just browse the articles that other people import. But it’s a good thing to&nbsp;
+					know how to do in case you ever want to import a specific article to Readup.</p>
 			},
 			{
-				question: "Can we fly to mars yet?",
-				answer: <p>Maybe. Do you want a cookie?</p>
+				question: "Can I import anything I want?",
+				answer: <p>Yep! Readup doesn’t enable you to bypass paywalls, but you can import paid articles&nbsp;
+					(for example, from the Wall Street Journal) if you pay for them separately.</p>
 			},
 			{
-				question: "How far is asgaard?",
-				answer: <p>Maybe. Do you want a cookie?</p>
+				question: "How do I import articles to Readup on my iPhone?",
+				answer: <p>The Readup share extension in iOS makes it easy to import articles to Readup with just a few clicks.&nbsp;
+					When you are viewing an article you want to import (for example in the Safari app) just click the share extension&nbsp;
+					(a square with an up arrow) and then click the Readup icon. (Note: If you can’t find the Readup icon, you might need to click “More.”)&nbsp;
+					The article will appear at the top of your Starred list in My Reads. </p>
 			},
 			{
-				question: "Do I have to pay?",
-				answer: <p>Maybe. Do you want a cookie?</p>
+				question: "How do I import articles to Readup on my laptop or computer?",
+				answer: <p>Just click the browser button. When you view an article in Reader Mode, it will automatically be saved to&nbsp;
+					the History section of My Reads.</p>
 			},
+		]
+	},
+	{
+		title: "Reading on Readup",
+		questions: [
+			{
+				question: "Why didn’t I get credit for something I read?",
+				answer: <p>Make sure that you’re reading in Reader Mode in order to get credit.</p>
+			},
+			{
+				question: "Am I “reading” too fast?",
+				answer: <p>Maybe. Fast reading is okay, but Readup won’t give you credit for skimming or scanning. If you think that Readup isn’t&nbsp;
+					giving you credit for articles you’re really reading, please send us an email: <a href="mailto:support@readup.com">support@readup.com</a></p>
+			},
+			{
+				question: "What if a specific article doesn’t work?",
+				answer: <p>Please flag any article errors by clicking the small flag in the top right corner of Reader Mode.</p>
+			}
+		]
+	},
+	{
+		title: "Privacy",
+		questions: [
+			{
+				question: "Does Readup track my reading?",
+				answer: <p>Yes. But it is completely secure and anonymous. Our <a href="https://readup.com/privacy">Privacy Policy</a> is short and sweet&nbsp;
+				and we highly suggest you read it.</p>
+			},
+			{
+				question: "Will other people know what I’ve been reading?",
+				answer: <p>Not by default. After you finish an article, you can choose to post it publicly.&nbsp;
+					Otherwise, all of your reading stays totally private.</p>
+			},
+			{
+				question: "Can I delete my account?",
+				answer: <p>Yes. Just send an email to <a href="mailto:support@readup.com">support@readup.com</a></p>
+			}
+		]
+	},
+	{
+		title: "Misc",
+		questions: [
+			{
+				question: "Can I change my username?",
+				answer: <p>No. If you want a new username, you need to create a new account.</p>
+			},
+			{
+				question: "Do you have an Android app?",
+				// todo: add get notified element here!
+				answer: <p>Not yet, but we will soon!</p>
+			},
+			{
+				question: "Who started Readup?",
+				// todo: make onViewProfile available in this page
+				answer: (<p>Two friends, <a href="https://billloundy.com/">Bill Loundy</a> and <a href="https://jeffcamera.com/">Jeff Camera</a>.&nbsp;
+					(We love reading and you’ll often see us in the comments. We’re <a href="/@bill">@bill</a> and <a href="/@jeff">@jeff</a>.)</p>)
+			},
+			{
+				question: "How does Readup make money? ",
+				// todo: make onViewProfile available in this page
+				answer: (<p>Currently, Readup doesn’t make money. And if you join now you’ll stay free for life.&nbsp;
+					But, soon, new Readers will have to pay to read on Readup.</p>)
+			},
+			{
+				question: "Are you hiring?",
+				// todo: make onViewProfile available in this page
+				answer: <p>Yes! Email us if you’re interested in joining the team: <a href="mailto:support@readup.com">support@readup.com</a></p>
+			},
+			{
+				question: "Does Readup work with publishers?",
+				answer: <p>Not yet, but we plan to. If you are a publisher and you’d like to have a conversation, don’t hesitate to reach out:&nbsp;
+					<a href="mailto:support@readup.com">support@readup.com</a></p>
+			}
 		]
 	}
 ];
@@ -98,6 +178,32 @@ const faqPage = () => {
 	// 				[fQ.title]: fQ.questions.reduce((qAcc, q) => ({...qAcc, [q.question]: false}), {})
 	// 			})
 	// 		, {}));
+
+	// TODO: bumping fixed sidebar
+	// const [fixed, setFixed] = React.useState(false);
+	// React.useEffect(() => {
+	// 	// set up sidebar dynamic scroll listener
+	// 	const header = document.querySelector('header');
+	// 	const homeHero = document.querySelector('.home-hero_527aw5');
+	// 	const mainScrollContainer = document.querySelector('main .screen');
+	// 	if (header && homeHero && mainScrollContainer) {
+	// 		const headerHeight = header.offsetHeight;
+	// 		const homeHeroHeight = header.offsetHeight;
+	// 		mainScrollContainer.addEventListener('scroll', (e) => {
+	// 			const scrollOffset = mainScrollContainer.scrollTop;
+	// 			const sidebar = document.querySelector('.sidebar');
+	// 			if (scrollOffset > homeHeroHeight && !fixed) {
+	// 				(sidebar as unknown as ElementCSSInlineStyle).style.position = 'fixed';
+	// 				(sidebar as unknown as ElementCSSInlineStyle).style.top = headerHeight + 40 + 'px';
+	// 				setFixed(true);
+	// 			} else if (fixed === true) {
+	// 				(sidebar as unknown as ElementCSSInlineStyle).style.position = 'static';
+	// 				(sidebar as unknown as ElementCSSInlineStyle).style.top = 0 + 'px';
+	// 				setFixed(false)
+	// 			}
+	// 		});
+	// 	}
+	// }, []);
 
 	return (
 		<div className="faq-page_35vamf">
