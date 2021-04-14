20f263c2687095ecb39b4eb0e5fd7512ea9a04f1
Jeff <jeff@jeffcamera.com>
2021-04-14 10:34:18 -0400

    added authorProfile to Profile

--- src/common/models/social/Profile.ts
+++ src/common/models/social/Profile.ts
@@ -1,9 +1,11 @@
 import LeaderboardBadge from '../LeaderboardBadge';
+import AuthorProfile from '../authors/AuthorProfile';
 
 export default interface Profile {
 	userName: string,
 	isFollowed: boolean,
 	leaderboardBadge: LeaderboardBadge,
 	followeeCount: number,
-	followerCount: number
+	followerCount: number,
+	authorProfile: AuthorProfile | null
 }
