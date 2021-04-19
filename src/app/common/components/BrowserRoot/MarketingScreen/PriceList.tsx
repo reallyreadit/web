1ffa03e6a3bb9ff99b67434bfae9f7d8fe7bdfa7
Jeff <jeff@jeffcamera.com>
2021-04-19 18:01:30 -0400

    added pricing panel to marketing screen

---
+++ src/app/common/components/BrowserRoot/MarketingScreen/PriceList.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { Props as PriceListItemProps, PriceListItem } from './PriceList/PriceListItem';
+
+interface Props {
+	prices: PriceListItemProps[]
+}
+
+export const PriceList: React.SFC<Props> = (props: Props) => (
+	<ol className="price-list_eorvmi">
+		{props.prices.map(
+			price => (
+				<PriceListItem
+					amount={price.amount}
+					description={price.description}
+					key={price.amount}
+					selected={price.selected}
+					title={price.title}
+				/>
+			)
+		)}
+	</ol>
+);
