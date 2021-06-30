1fcd12af07c9a2afa4b8218d62da0e5a9bd50316
Jeff <jeff@jeffcamera.com>
2021-06-30 10:24:59 -0400

    added donationRecipientName to earnings report

--- src/common/models/subscriptions/AuthorEarningsReport.ts
+++ src/common/models/subscriptions/AuthorEarningsReport.ts
@@ -2,6 +2,7 @@ export interface AuthorEarningsReport {
 	authorName: string,
 	authorSlug: string,
 	userAccountName: string | null,
+	donationRecipientName: string | null,
 	minutesRead: number,
 	amountEarned: number
 }
