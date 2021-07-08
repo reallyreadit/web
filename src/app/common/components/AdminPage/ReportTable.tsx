f78fdd2beddd0d4907df69f6d70c1bebd1c193d9
Jeff <jeff@jeffcamera.com>
2021-07-08 16:14:55 -0400

    created reusable ReportTable from RevenueReport

---
+++ src/app/common/components/AdminPage/ReportTable.tsx
@@ -0,0 +1,158 @@
+import * as React from 'react';
+import InputControl from '../../../../common/components/controls/InputControl';
+import Button from '../../../../common/components/Button';
+import { FetchFunctionWithParams } from '../../serverApi/ServerApi';
+import DateRangeQuery from '../../../../common/models/analytics/DateRangeQuery';
+import Fetchable from '../../../../common/Fetchable';
+import AsyncTracker from '../../../../common/AsyncTracker';
+
+interface Header {
+	name: string,
+	colSpan?: number
+}
+interface Props<TData> {
+	title: string,
+	initialStartDate: string,
+	initialEndDate: string,
+	headers: Header[][],
+	onFetchData: FetchFunctionWithParams<DateRangeQuery, TData>,
+	onRenderBody: (data: TData, columnCount: number) => React.ReactNode,
+	onRenderChart?: (data: TData) => React.ReactNode,
+	onRenderFooter?: (data: TData, columnCount: number) => React.ReactNode
+};
+interface State<TData> {
+	startDate: string,
+	endDate: string,
+	data: Fetchable<TData> | null
+}
+export class ReportTable<TData> extends React.Component<Props<TData>, State<TData>> {
+	private readonly _asyncTracker = new AsyncTracker();
+	private readonly _runReport = (event: React.MouseEvent) => {
+		event.preventDefault();
+		this.setState({
+			data: this.props.onFetchData(
+				{
+					startDate: this.state.startDate,
+					endDate: this.state.endDate
+				},
+				this._asyncTracker.addCallback(
+					data => {
+						this.setState({
+							data
+						});
+					}
+				)
+			)
+		});
+	};
+	private readonly _setStartDate = (value: string) => {
+		this.setState({
+			startDate: value
+		});
+	};
+	private readonly _setEndDate = (value: string) => {
+		this.setState({
+			endDate: value
+		});
+	};
+	constructor(props: Props<TData>) {
+		super(props);
+		this.state = {
+			data: null,
+			startDate: props.initialStartDate,
+			endDate: props.initialEndDate
+		};
+	}
+	public componentWillUnmount() {
+		this._asyncTracker.cancelAll();
+	}
+	public render() {
+		const columnCount = this.props.headers[0].reduce(
+			(sum, header) => sum + (header.colSpan ?? 1),
+			0
+		);
+		let
+			chart: React.ReactNode | undefined,
+			body: React.ReactNode,
+			footer: React.ReactNode | undefined;
+		if (this.state.data?.value) {
+			if (this.props.onRenderChart) {
+				chart = this.props.onRenderChart(this.state.data.value);
+			}
+			body = this.props.onRenderBody(this.state.data.value, columnCount);
+			if (this.props.onRenderFooter) {
+				footer = this.props.onRenderFooter(this.state.data.value, columnCount);
+			}
+		} else {
+			body = (
+				<tbody>
+					<tr>
+						<td colSpan={columnCount}>
+							{this.state.data ?
+								this.state.data.isLoading ?
+									'Loading...' :
+									'Error loading data.' :
+								'Click "Run Report" to load data.'}
+						</td>
+					</tr>
+				</tbody>
+			);
+		}
+		return (
+			<div className="report-table_gitemz">
+				<table>
+					<caption>
+						<div className="content">
+							<div className="header">
+								<label>{this.props.title}</label>
+								<form>
+									<InputControl
+										type="text"
+										label="Start Date"
+										value={this.state.startDate}
+										onChange={this._setStartDate}
+									/>
+									<InputControl
+										type="text"
+										label="End Date"
+										value={this.state.endDate}
+										onChange={this._setEndDate}
+									/>
+									<Button
+										onClick={this._runReport}
+										text="Run Report"
+									/>
+								</form>
+							</div>
+							{chart ?
+								<div className="chart">
+									{chart}
+								</div> :
+								null}
+						</div>
+					</caption>
+					<thead>
+						{this.props.headers.map(
+							row => (
+								<tr key={row.map(header => header.name).join(',')}>
+									{row.map(
+										header => (
+											<th
+												colSpan={header.colSpan ?? 1}
+												key={header.name}
+											>
+												{header.name}
+											</th>
+										)
+									)}
+								</tr>
+							)
+						)}
+					</thead>
+					{body}
+					{footer}
+				</table>
+			</div>
+		);
+	}
+}
--- src/app/common/components/AdminPage/RevenueReport.tsx
+++ src/app/common/components/AdminPage/RevenueReport.tsx
@@ -1,30 +1,21 @@
 import * as React from 'react';
-import AsyncTracker from '../../../../common/AsyncTracker';
 import { FetchFunctionWithParams } from '../../serverApi/ServerApi';
 import DateRangeQuery from '../../../../common/models/analytics/DateRangeQuery';
 import { RevenueReportResponse, RevenueReportLineItem } from '../../../../common/models/analytics/RevenueReport';
-import Fetchable from '../../../../common/Fetchable';
 import { DateTime } from 'luxon';
-import InputControl from '../../../../common/components/controls/InputControl';
-import Button from '../../../../common/components/Button';
 import { formatCurrency } from '../../../../common/format';
 import SubscriptionProvider from '../../../../common/models/subscriptions/SubscriptionProvider';
 import * as classNames from 'classnames';
 import { ResponsiveContainer, Area, XAxis, YAxis, Tooltip, ComposedChart, Line } from 'recharts';
 import { ChartKey } from '../controls/ChartKey';
+import { ReportTable } from './ReportTable';
 
 interface Props {
 	onGetRevenueReport: FetchFunctionWithParams<DateRangeQuery, RevenueReportResponse>
 }
-interface State {
-	startDate: string,
-	endDate: string,
-	response: Fetchable<RevenueReportResponse> | null
-}
-interface ChartDataPoint {
-	xLabel: string,
-	appleAmount: number,
-	stripeAmount: number
+interface LineItemPeriodGroup {
+	period: string,
+	items: RevenueReportLineItem[]
 }
 
 enum Price {
@@ -33,9 +24,233 @@ enum Price {
 	SuperReader = 'Super Reader'
 }
 
+const headers = [
+	[
+		{
+			name: ''
+		},
+		{
+			name: 'Budget ($4.99)',
+			colSpan: 2
+		},
+		{
+			name: 'Reader ($14.99)',
+			colSpan: 2
+		},
+		{
+			name: 'Super Reader ($24.99)',
+			colSpan: 2
+		},
+		{
+			name: 'Custom'
+		},
+		{
+			name: ''
+		},
+		{
+			name: ''
+		}
+	],
+	[
+		{
+			name: 'Period'
+		},
+		{
+			name: 'Apple'
+		},
+		{
+			name: 'Stripe'
+		},
+		{
+			name: 'Apple'
+		},
+		{
+			name: 'Stripe'
+		},
+		{
+			name: 'Apple'
+		},
+		{
+			name: 'Stripe'
+		},
+		{
+			name: 'Stripe'
+		},
+		{
+			name: 'Total'
+		},
+		{
+			name: 'MRR'
+		}
+	]
+];
+
+function createPeriodGroups(data: RevenueReportResponse) {
+	return data.lineItems
+		.reduce<LineItemPeriodGroup[]>(
+			(groups, item) => {
+				const group = groups.find(
+					group => group.period === item.period
+				);
+				if (group) {
+					group.items.push(item);
+				} else {
+					groups.push({
+						period: item.period,
+						items: [item]
+					});
+				}
+				return groups;
+			},
+			[]
+		)
+		.sort(
+			(a, b) => b.period < a.period ?
+				-1 :
+				b.period > a.period ?
+					1 :
+					0
+		);
+}
 function getCustomItems(items: RevenueReportLineItem[]) {
 	return items.filter(
 		item => item.priceAmount > 0 && !item.priceName
+	);
+}
+function renderBody(data: RevenueReportResponse, columnCount: number) {
+	if (!data.lineItems.length) {
+		return (
+			<tbody>
+				<tr>
+					<td colSpan={columnCount}>No line items found.</td>
+				</tr>
+			</tbody>
+		);
+	}
+	return createPeriodGroups(data)
+		.map(
+			group => {
+				const
+					groupTotal = sumNetAmount(group.items),
+					customTotal = sumNetAmount(
+						getCustomItems(group.items)
+					),
+					monthlyRecurringLineItem = data.monthlyRecurringRevenueLineItems.find(
+						item => item.period === group.period
+					);
+				return (
+					<tr>
+						<td className="align-center">{group.period.replace(/T00:00:00$/, '')}</td>
+						{renderProviderPriceCells(group.items, Price.Budget)}
+						{renderProviderPriceCells(group.items, Price.Reader)}
+						{renderProviderPriceCells(group.items, Price.SuperReader)}
+						<td className={classNames('align-right', { 'null': customTotal === 0 })}>
+							{formatCurrency(customTotal)}
+						</td>
+						<td className={classNames('align-right', { 'null': groupTotal === 0 })}>
+							<strong>
+								{formatCurrency(groupTotal)}
+							</strong>
+						</td>
+						<td className="align-right">
+							{formatCurrency(monthlyRecurringLineItem.amount)}
+						</td>
+					</tr>
+				);
+			}
+		);
+}
+function renderChart(data: RevenueReportResponse) {
+	if (!data.lineItems.length) {
+		return null;
+	}
+	const chartPoints = createPeriodGroups(data)
+		.reverse()
+		.map(
+			group => ({
+				xLabel: DateTime
+					.fromISO(group.period)
+					.toFormat('L/d'),
+				appleAmount: sumNetAmount(
+					group.items.filter(
+						item => item.provider === SubscriptionProvider.Apple
+					)
+				),
+				stripeAmount: sumNetAmount(
+					group.items.filter(
+						item => item.provider === SubscriptionProvider.Stripe
+					)
+				),
+				monthlyRecurringRevenue: data.monthlyRecurringRevenueLineItems
+					.find(
+						item => item.period === group.period
+					)
+					.amount
+			})
+		);
+	return (
+		<>
+			<ResponsiveContainer aspect={2}>
+				<ComposedChart
+					data={chartPoints}
+					margin={{ top: 10, right: 20, left: 10 }}
+					onClick={
+						() => {
+							// need to have an onClick handler in order for touch events to display tooltips
+						}
+					}
+				>
+					<Area
+						dataKey="appleAmount"
+						fill="#c3d7ef"
+						name="Apple"
+						stackId="1"
+						stroke="#9bbde4"
+						type="monotone"
+						yAxisId="dailySalesTotal"
+					/>
+					<Area
+						dataKey="stripeAmount"
+						fill="#73a3d9"
+						name="Stripe"
+						stackId="1"
+						stroke="#4b88ce"
+						type="monotone"
+						yAxisId="dailySalesTotal"
+					/>
+					<Line
+						dataKey="monthlyRecurringRevenue"
+						dot={false}
+						name="MRR"
+						stroke="#ff0000"
+						strokeWidth={3}
+						type="monotone"
+						yAxisId="monthlyRecurringRevenue"
+					/>
+					<XAxis
+						dataKey="xLabel"
+						minTickGap={1}
+					/>
+					<YAxis
+						minTickGap={1}
+						orientation="left"
+						tickFormatter={formatCurrency}
+						yAxisId="dailySalesTotal"
+					/>
+					<YAxis
+						minTickGap={1}
+						orientation="right"
+						tickFormatter={formatCurrency}
+						yAxisId="monthlyRecurringRevenue"
+					/>
+					<Tooltip formatter={formatCurrency} />
+				</ComposedChart>
+			</ResponsiveContainer>
+			<ChartKey
+				item1="Apple"
+				item2="Stripe"
+			/>
+		</>
 	);
 }
 function renderProviderPriceCells(items: RevenueReportLineItem[], price: Price, format: 'normal' | 'total' = 'normal') {
@@ -68,19 +283,22 @@ function renderProviderPriceCells(items: RevenueReportLineItem[], price: Price, 
 		</React.Fragment>
 	);
 }
-function renderFooter(items: RevenueReportLineItem[]) {
+function renderFooter(data: RevenueReportResponse) {
+	if (!data.lineItems.length) {
+		return null;
+	}
 	const
 		customTotal = sumNetAmount(
-			getCustomItems(items)
+			getCustomItems(data.lineItems)
 		),
-		grandTotal = sumNetAmount(items);
+		grandTotal = sumNetAmount(data.lineItems);
 	return (
 		<tfoot>
 			<tr>
 				<th>Total</th>
-				{renderProviderPriceCells(items, Price.Budget, 'total')}
-				{renderProviderPriceCells(items, Price.Reader, 'total')}
-				{renderProviderPriceCells(items, Price.SuperReader, 'total')}
+				{renderProviderPriceCells(data.lineItems, Price.Budget, 'total')}
+				{renderProviderPriceCells(data.lineItems, Price.Reader, 'total')}
+				{renderProviderPriceCells(data.lineItems, Price.SuperReader, 'total')}
 				<td
 					className={classNames('align-right', { 'null': customTotal === 0 })}
 				>
@@ -105,39 +323,9 @@ function sumNetAmount(items: RevenueReportLineItem[]) {
 	);
 }
 
-export class RevenueReport extends React.Component<Props, State> {
-	private readonly _asyncTracker = new AsyncTracker();
-	private readonly _runReport = (event: React.MouseEvent) => {
-		event.preventDefault();
-		this.setState({
-			response: this.props.onGetRevenueReport(
-				{
-					startDate: this.state.startDate,
-					endDate: this.state.endDate
-				},
-				this._asyncTracker.addCallback(
-					response => {
-						this.setState({
-							response
-						});
-					}
-				)
-			)
-		});
-	};
-	private readonly _setStartDate = (value: string) => {
-		this.setState({
-			startDate: value
-		});
-	};
-	private readonly _setEndDate = (value: string) => {
-		this.setState({
-			endDate: value
-		});
-	};
-	private readonly _showToolTip = () => {
-		// need to have an onClick handler in order for touch events to display tooltips
-	};
+export class RevenueReport extends React.Component<Props> {
+	private readonly _initialStartDate: string;
+	private readonly _initialEndDate: string;
 	constructor(props: Props) {
 		super(props);
 		const
@@ -149,247 +337,31 @@ export class RevenueReport extends React.Component<Props, State> {
 					day: localNow.day,
 					zone: 'utc'
 				});
-		this.state = {
-			startDate: localNowDate
-				.minus({
-					days: 30
-				})
-				.toISO({
-					suppressMilliseconds: true,
-					includeOffset: false
-				}),
-			endDate: localNowDate.toISO({
-					suppressMilliseconds: true,
-					includeOffset: false
-				}),
-			response: null
-		};
-	}
-	public componentWillUnmount() {
-		this._asyncTracker.cancelAll();
+		this._initialStartDate = localNowDate
+			.minus({
+				days: 30
+			})
+			.toISO({
+				suppressMilliseconds: true,
+				includeOffset: false
+			});
+		this._initialEndDate = localNowDate.toISO({
+			suppressMilliseconds: true,
+			includeOffset: false
+		});
 	}
 	public render() {
-		const columnCount = 10;
-		let
-			periodGroups: { period: string, items: RevenueReportLineItem[] }[] | null,
-			chartPoints: ChartDataPoint[] | null;
-		if (this.state.response?.value?.lineItems.length) {
-			periodGroups = this.state.response.value.lineItems
-				.reduce<{ period: string, items: RevenueReportLineItem[] }[]>(
-					(groups, item) => {
-						const group = groups.find(
-							group => group.period === item.period
-						);
-						if (group) {
-							group.items.push(item);
-						} else {
-							groups.push({
-								period: item.period,
-								items: [item]
-							});
-						}
-						return groups;
-					},
-					[]
-				)
-				.sort(
-					(a, b) => b.period < a.period ?
-						-1 :
-						b.period > a.period ?
-							1 :
-							0
-				);
-			chartPoints = periodGroups
-				.slice()
-				.reverse()
-				.map(
-					group => ({
-						xLabel: DateTime
-							.fromISO(group.period)
-							.toFormat('L/d'),
-						appleAmount: sumNetAmount(
-							group.items.filter(
-								item => item.provider === SubscriptionProvider.Apple
-							)
-						),
-						stripeAmount: sumNetAmount(
-							group.items.filter(
-								item => item.provider === SubscriptionProvider.Stripe
-							)
-						),
-						monthlyRecurringRevenue: this.state.response.value.monthlyRecurringRevenueLineItems
-							.find(
-								item => item.period === group.period
-							)
-							.amount
-					})
-				);
-		}
 		return (
-			<div className="revenue-report_tbgqil">
-				<table>
-					<caption>
-						<div className="content">
-							<div className="header">
-								<label>Revenue Report</label>
-								<form>
-									<InputControl
-										type="text"
-										label="Start Date"
-										value={this.state.startDate}
-										onChange={this._setStartDate}
-									/>
-									<InputControl
-										type="text"
-										label="End Date"
-										value={this.state.endDate}
-										onChange={this._setEndDate}
-									/>
-									<Button
-										onClick={this._runReport}
-										text="Run Report"
-									/>
-								</form>
-							</div>
-							{chartPoints ?
-								<div className="chart">
-									<ResponsiveContainer aspect={2}>
-										<ComposedChart
-											data={chartPoints}
-											margin={{ top: 10, right: 20, left: 10 }}
-											onClick={this._showToolTip}
-										>
-											<Area
-												dataKey="appleAmount"
-												fill="#c3d7ef"
-												name="Apple"
-												stackId="1"
-												stroke="#9bbde4"
-												type="monotone"
-												yAxisId="dailySalesTotal"
-											/>
-											<Area
-												dataKey="stripeAmount"
-												fill="#73a3d9"
-												name="Stripe"
-												stackId="1"
-												stroke="#4b88ce"
-												type="monotone"
-												yAxisId="dailySalesTotal"
-											/>
-											<Line
-												dataKey="monthlyRecurringRevenue"
-												dot={false}
-												name="MRR"
-												stroke="#ff0000"
-												strokeWidth={3}
-												type="monotone"
-												yAxisId="monthlyRecurringRevenue"
-											/>
-											<XAxis
-												dataKey="xLabel"
-												minTickGap={1}
-											/>
-											<YAxis
-												minTickGap={1}
-												orientation="left"
-												tickFormatter={formatCurrency}
-												yAxisId="dailySalesTotal"
-											/>
-											<YAxis
-												minTickGap={1}
-												orientation="right"
-												tickFormatter={formatCurrency}
-												yAxisId="monthlyRecurringRevenue"
-											/>
-											<Tooltip formatter={formatCurrency} />
-										</ComposedChart>
-									</ResponsiveContainer>
-									<ChartKey
-										item1="Apple"
-										item2="Stripe"
-									/>
-								</div> :
-								null}
-						</div>
-					</caption>
-					<thead>
-						<tr>
-							<th></th>
-							<th colSpan={2}>Budget ($4.99)</th>
-							<th colSpan={2}>Reader ($14.99)</th>
-							<th colSpan={2}>Super Reader ($24.99)</th>
-							<th>Custom</th>
-							<th></th>
-							<th></th>
-						</tr>
-						<tr>
-							<th>Period</th>
-							<th>Apple</th>
-							<th>Stripe</th>
-							<th>Apple</th>
-							<th>Stripe</th>
-							<th>Apple</th>
-							<th>Stripe</th>
-							<th>Stripe</th>
-							<th>Total</th>
-							<th>MRR</th>
-						</tr>
-					</thead>
-					<tbody>
-						{this.state.response ?
-							this.state.response.value ?
-								this.state.response.value.lineItems.length ?
-									periodGroups.map(
-										group => {
-											const
-												groupTotal = sumNetAmount(group.items),
-												customTotal = sumNetAmount(
-													getCustomItems(group.items)
-												),
-												monthlyRecurringLineItem = this.state.response.value.monthlyRecurringRevenueLineItems.find(
-													item => item.period === group.period
-												);
-											return (
-												<tr>
-													<td className="align-center">{group.period.replace(/T00:00:00$/, '')}</td>
-													{renderProviderPriceCells(group.items, Price.Budget)}
-													{renderProviderPriceCells(group.items, Price.Reader)}
-													{renderProviderPriceCells(group.items, Price.SuperReader)}
-													<td className={classNames('align-right', { 'null': customTotal === 0 })}>
-														{formatCurrency(customTotal)}
-													</td>
-													<td className={classNames('align-right', { 'null': groupTotal === 0 })}>
-														<strong>
-															{formatCurrency(groupTotal)}
-														</strong>
-													</td>
-													<td className="align-right">
-														{formatCurrency(monthlyRecurringLineItem.amount)}
-													</td>
-												</tr>
-											);
-										}
-									) :
-									<tr>
-										<td colSpan={columnCount}>No line items found.</td>
-									</tr> :
-								this.state.response.isLoading ?
-									<tr>
-										<td colSpan={columnCount}>Loading...</td>
-									</tr> :
-									<tr>
-										<td colSpan={columnCount}>Error running report.</td>
-									</tr> :
-							<tr>
-								<td colSpan={columnCount}>Click "Run Report" to run report.</td>
-							</tr>}
-					</tbody>
-					{this.state.response?.value?.lineItems.length ?
-						renderFooter(this.state.response.value.lineItems) :
-						null}
-				</table>
-			</div>
+			<ReportTable
+				title="Revenue Report"
+				initialStartDate={this._initialStartDate}
+				initialEndDate={this._initialEndDate}
+				headers={headers}
+				onFetchData={this.props.onGetRevenueReport}
+				onRenderBody={renderBody}
+				onRenderChart={renderChart}
+				onRenderFooter={renderFooter}
+			/>
 		);
 	}
 }
