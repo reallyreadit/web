f07db2ed395bc9b2bb1d6c54056004b3e85cc407
Jeff <jeff@jeffcamera.com>
2021-07-15 18:26:25 -0400

    added AuthorEarningsReportStatus

--- src/common/models/subscriptions/AuthorEarningsReport.ts
+++ src/common/models/subscriptions/AuthorEarningsReport.ts
@@ -4,7 +4,15 @@ export interface AuthorEarningsReport {
 	userAccountName: string | null,
 	donationRecipientName: string | null,
 	minutesRead: number,
-	amountEarned: number
+	amountEarned: number,
+	status: AuthorEarningsReportStatus
+}
+export enum AuthorEarningsReportStatus {
+	ApproachingMinimum = 1,
+	NotYetContacted = 2,
+	Contacted = 3,
+	AuthorPaidOut = 4,
+	DonationPaidOut = 5
 }
 export interface AuthorsEarningsReportResponse {
 	lineItems: AuthorEarningsReport[]
