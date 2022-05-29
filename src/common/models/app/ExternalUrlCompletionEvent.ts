2a757174eb1c493241c65fa2b9018fbdb13160b4
Jeff <jeff@jeffcamera.com>
2022-05-29 18:30:28 -0400

    added copyright and license notice to source files

--- src/common/models/app/ExternalUrlCompletionEvent.ts
+++ src/common/models/app/ExternalUrlCompletionEvent.ts
@@ -1,3 +1,13 @@
+// Copyright (C) 2022 reallyread.it, inc.
+// 
+// This file is part of Readup.
+// 
+// Readup is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License version 3 as published by the Free Software Foundation.
+// 
+// Readup is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License version 3 along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+
 export interface ExternalUrlCompletionEvent {
 
 }
