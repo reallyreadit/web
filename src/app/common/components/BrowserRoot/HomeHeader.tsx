eab72c285aecc9c2d7aead9af224e33d22c68a8c
Thor Galle <thorgalle@gmail.com>
2022-05-30 15:48:51 +0300

    Add a GitHub icon on the home page

--- src/app/common/components/BrowserRoot/HomeHeader.tsx
+++ src/app/common/components/BrowserRoot/HomeHeader.tsx
@@ -1,11 +1,11 @@
 // Copyright (C) 2022 reallyread.it, inc.
-// 
+//
 // This file is part of Readup.
-// 
+//
 // Readup is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License version 3 as published by the Free Software Foundation.
-// 
+//
 // Readup is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License version 3 along with Foobar. If not, see <https://www.gnu.org/licenses/>.
 
 import * as React from 'react';
@@ -108,6 +108,9 @@ export default class HomeHeader extends React.PureComponent<Props, State> {
 								{link.linkText}
 							</Link>)
 						}
+						<Link href="https://github.com/reallyreadit" onClick={this.props.onNavTo}>
+							<Icon name="github"></Icon>
+						</Link>
 						<Button
 							text="Download App"
 							size="large"
--- src/common/components/Icon.tsx
+++ src/common/components/Icon.tsx
@@ -1,11 +1,11 @@
 // Copyright (C) 2022 reallyread.it, inc.
-// 
+//
 // This file is part of Readup.
-// 
+//
 // Readup is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License version 3 as published by the Free Software Foundation.
-// 
+//
 // Readup is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU Affero General Public License version 3 along with Foobar. If not, see <https://www.gnu.org/licenses/>.
 
 import * as React from 'react';
@@ -13,7 +13,7 @@ import classNames from 'classnames';
 import { ClassValue } from 'classnames/types';
 import AlertBadge from './AlertBadge';
 
-export type IconName = 'android' | 'apple' | 'arrow-left' | 'arrow-down' | 'arrow-right' | 'arrow-ne' | 'arrow-up' | 'article-details-star' | 'at-sign' | 'backward' | 'bell' | 'binoculars' | 'bookmark' | 'box' | 'cancel' | 'candy' | 'charity' | 'chart' | 'checkmark' | 'chevron-down' | 'chevron-left' | 'chevron-right' | 'chevron-up' | 'chrome' |'clock' | 'comments' | 'cross' | 'dollar' | 'earth' | 'edge' | 'email' | 'envelope' | 'equalizer' | 'exclamation' | 'fire' | 'flag' | 'firefox' | 'forbid' | 'gear2' | 'graduation' | 'group-circle' | 'hourglass' | 'history-simple' | 'internet' | 'link' | 'linkedin' | 'linux' | 'locked' | 'medal' | 'megaphone' | 'menu' | 'menu2' | 'money-pouch' | 'padlock' | 'paper-plane' | 'phone' | 'pie-chart' | 'piggy-bank' | 'plus' | 'podium' | 'power' | 'question-circle' | 'quill' | 'rating-seal' | 'refresh' | 'refresh2' | 'rocket' | 'safari' | 'share' | 'share-android' | 'spinner' | 'star' | 'trophy' | 'twitter' | 'user' | 'verified-user' | 'warning' | 'windows' | 'write';
+export type IconName = 'android' | 'apple' | 'arrow-left' | 'arrow-down' | 'arrow-right' | 'arrow-ne' | 'arrow-up' | 'article-details-star' | 'at-sign' | 'backward' | 'bell' | 'binoculars' | 'bookmark' | 'box' | 'cancel' | 'candy' | 'charity' | 'chart' | 'checkmark' | 'chevron-down' | 'chevron-left' | 'chevron-right' | 'chevron-up' | 'chrome' |'clock' | 'comments' | 'cross' | 'dollar' | 'earth' | 'edge' | 'email' | 'envelope' | 'equalizer' | 'exclamation' | 'fire' | 'flag' | 'firefox' | 'forbid' | 'gear2' | 'github' | 'graduation' | 'group-circle' | 'hourglass' | 'history-simple' | 'internet' | 'link' | 'linkedin' | 'linux' | 'locked' | 'medal' | 'megaphone' | 'menu' | 'menu2' | 'money-pouch' | 'padlock' | 'paper-plane' | 'phone' | 'pie-chart' | 'piggy-bank' | 'plus' | 'podium' | 'power' | 'question-circle' | 'quill' | 'rating-seal' | 'refresh' | 'refresh2' | 'rocket' | 'safari' | 'share' | 'share-android' | 'spinner' | 'star' | 'trophy' | 'twitter' | 'user' | 'verified-user' | 'warning' | 'windows' | 'write';
 export default (props: {
 	badge?: number | false,
 	display?: 'inline' | 'block',
