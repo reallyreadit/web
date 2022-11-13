8434574e3ef40a0b8776b5a0f1f488a6a9b37ab1
Thor Galle <thorgalle@gmail.com>
2022-11-13 16:25:03 +0000

    Delete unused component hosts

--- src/extension/content-scripts/reader/ExtensionComponentHost.ts
+++
@@ -1,7 +0,0 @@
-import ComponentHost from '../../../common/shadowDom/ComponentHost';
-
-export default abstract class ExtensionComponentHost<Services, State> extends ComponentHost<Services, State> {
-	protected getStylesheetUrl() {
-		return chrome.runtime.getURL('/content-scripts/reader/bundle.css');
-	}
-}
