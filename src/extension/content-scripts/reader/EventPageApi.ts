48ed4885ec89deda9346a0f6613bb405e33ba225
Jeff <jeff@jeffcamera.com>
2022-11-20 20:53:01 -0500

    prelim fix for crosstalk between reader scripts

--- src/extension/content-scripts/reader/EventPageApi.ts
+++ src/extension/content-scripts/reader/EventPageApi.ts
@@ -82,6 +82,11 @@ export default class EventPageApi {
 	}) {
 		chrome.runtime.onMessage.addListener(
 			(message, sender, sendResponse) => {
+				// Don't answer messages from other content scripts.
+				// TODO: Explore full consequences of running this messaging service in a chrome-extension://... page instead of a regular http(s)://... web page and refactor as required.
+				if (message.from === 'readerContentScript') {
+					return;
+				}
 				switch (message.type) {
 					case 'articleUpdated':
 						handlers.onArticleUpdated(message.data);
